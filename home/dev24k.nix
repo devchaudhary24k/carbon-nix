2ef21e6aed18a9fe5a04950d60848b2895d90b66
Dev Talan <84081651+devchaudhary24k@users.noreply.github.com>
2026-08-20 13:40:07 +0530

    Pin T3 service runtime environment

--- home/dev24k.nix
+++ home/dev24k.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   home = {
@@ -19,6 +19,16 @@
       "git/ignore".source = "${inputs.dotfiles}/home/git/.config/git/ignore";
       "nvim".source = inputs.lazyvim-starter;
       "starship.toml".source = "${inputs.dotfiles}/home/starship/.config/starship.toml";
+
+      # T3 starts from the lingering user systemd manager, not a login shell.
+      # Pin Nix Node/npm for its updater and pass nix-ld through to terminals
+      # so project-specific Node versions downloaded by fnm work on NixOS.
+      "systemd/user/t3code.service.d/10-nixos-compat.conf".text = ''
+        [Service]
+        Environment="PATH=${pkgs.nodejs_24}/bin:/run/wrappers/bin:/etc/profiles/per-user/dev24k/bin:/run/current-system/sw/bin"
+        Environment="NIX_LD=/run/current-system/sw/share/nix-ld/lib/ld.so"
+        Environment="NIX_LD_LIBRARY_PATH=/run/current-system/sw/share/nix-ld/lib"
+      '';
     };
   };
 
